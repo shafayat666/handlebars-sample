a472bbb4658216d2ef04a9927ea43bfc5dc18a99
Shafayat Mahin <shafayatmahin5@gmail.com>
2025-02-01 19:17:19 +0600

    feat/add basic layout

--- server.js
+++ server.js
@@ -9,14 +9,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
 
 // middlewars
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Handlebars.js Example",
+    message: "Hello World!",
+  });
 });
 
 app.listen(PORT, () => {
